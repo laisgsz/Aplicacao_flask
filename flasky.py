9207d27a5e237cb95e83fe7cacb22f04f7ac8cef
Lais Gabriele <161658694+laisgsz@users.noreply.github.com>
2025-11-28 20:35:27 -0300

    Refactor app creation with environment variable

--- flasky.py
+++ flasky.py
@@ -1,7 +1,10 @@
-from app import create_app
+import os
+# Importa do __init__.py (que é onde o create_app e db costumam estar nessa estrutura)
+from __init__ import create_app, db 
+# Importa o arquivo models.py direto
+from models import User, Role, Curso
 
-app = create_app('flasky.config.DevelopmentConfig')
-
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
     app.run(debug=True)
